5799c88a67791d5a22d2b3f5357fd74ecc13f076
jonathan_dreyer <jonathan.dreyer60@gmail.com>
2022-12-12 17:18:47 +0200

    day_8 & day_9 & day_10(not finished)

--- day_3.rb
+++ day_3.rb
@@ -1,77 +1,40 @@
-# data = File.open('test.txt').to_a
+# part 1
+ alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
 
-data = File.foreach('test.txt',"\n").to_a
+bags = File.open('day_3.txt').each_line.map { |line| line.chomp.chars }
 
-# p data
+compartmentalized_bags = bags.map { |bag| bag.each_slice(bag.size / 2).to_a }
 
-groups = data.each_slice(3).to_a
-
-groups.each do |d|
-
-new_array = d.each_slice(3).to_a
-
-p new_array
-
+common_letters = compartmentalized_bags.map do |compartment|
+  compartment[0] & compartment[1]
 end
 
-# matches = groups[0] & groups[1] & groups[3]
-# p matches
-# def position(letter)
-#   letter.ord - 'a'.ord + 1
-# end
+sum = 0
 
-# def a_position(letter)
-#   letter.ord - 'A'.ord + 1
-# end
+common_letters.flatten.each do |letter|
+  sum += alphabet.find_index(letter) + 1
+end
 
-# total_score = 0
+# part 2
 
-# data.each do |d|
-#   split_array = d.split("")
-#   new_array = split_array.each_slice( (split_array.size/2.0).floor ).to_a
-#   # p new_array[0]
-#   # p new_array[1]
-#   matches = new_array[0] & new_array[1]
-#   # p matches
-#   # new_array.each do |f|
-#   #   p f
-#   # end
-#   matches.each do |m|
-#     if m == m.capitalize
-#       a_amount = matches.map do |char| a_position(char) +26 end
-#       total_score += a_amount.join.to_f
-#     else
-#       amount = matches.map do |char| position(char) end
-#         total_score += amount.join.to_f
-#     end
-#   end
+data = File.read('day_3.txt')
 
-#   # alphabet = ('a'..'z').to_a
-#   # p matches.map { |d| alphabet.index(d) + 1 }
-#   # matches.each do |l|
-#   #   if l == alphabet.map
-#   #     p matches.map { |d| alphabet.index(d) + 1 }
-#   #   end
-#   # end
-#   # => ["A", "B", "C", "D", "E", ..., "Z"]
-#   # matches.map { |d| alphabet.index(d) + 1 + plus }
-#   # un = matches.map { |d| alphabet.index(d) + 1 }
-#   # p un
-# end
+groups = data.split.each_slice(3).to_a
 
-# p total_score
-# # array1 = [2,3,4,5,6,7] #does not have repeated values[2,2,3] and the values are sorted
-# # array2 = [2,5,7]
+array_groups = groups.map do |group|
+  group.map(&:split)
+end
 
-# # matches = array1 & array2
-# #  => [2, 5, 7]
+charred_array = array_groups.flatten.map(&:chars)
 
-# #  puts data
-# # p data
+badges = charred_array.each_slice(3).to_a.map do |array|
+  array[0] & array[1] & array[2]
+end
 
-# # plus = 9
-# # alphabet = ('A'..'Z').to_a
-# # # => ["A", "B", "C", "D", "E", ..., "Z"]
-# # # matches.map { |d| alphabet.index(d) + 1 + plus }
-# # un =matches.map { |d| alphabet.index(d) + 1 }
-# # p un
+second_sum = 0
+
+badges.flatten.each do |badge|
+  second_sum += alphabet.find_index(badge) + 1
+end
+
+p second_sum
